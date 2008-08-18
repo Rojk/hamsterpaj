2989638e119728b13650b498cc37f86b4a5709aa
Joar Wandborg <joar@hamsterpaj.net>
2008-08-18 18:55:03 +0200

    The base for synchrnoize.js was coded.
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- javascripts/synchronize.js
+++ javascripts/synchronize.js
@@ -2,19 +2,42 @@
 if(typeof(hp) == 'undefined'){ var hp=new Object(); }
 
 hp.synchronize = {
+	synchronize_interval: 20,
+	
 	synchronized_objects: new Array(),
 	init: function()
 	{
-		
+		setInterval('hp.synchronize.do_synchronizaion()', this.synchronize_interval * 1000);
 	},
 	
-	add_object: function()
+	add: function(params)
 	{
-		
+		this.synchronized_objects[params.handle] = params;
 	},
 	
+	do_synchronization: function()
+	{
+		var loader = hp.give_me_an_AJAX();
+		loader.onreadystatechange = function()
+		{
+			if(loader.readyState == 4 && loader.status == 200)
+			{
+				hp.synchronization.parse_response(eval('(' + loader.responseText + ')'));
+			}
+		}
+		loader.open('GET', '/ajax_gateways/synchronization.json.php', true);
+		loader.send(null);
+	},
 	
-	
-	
-	
+	parse_response: function(json_data)
+	{
+		for(var handle in json_data)
+		{
+			if(typeof(this.synchronized_objects[handle]) != 'undefined')
+			{
+				this.synchronized_objects[handle].json_data = json_data[handle];
+				eval(this.synchronized_objects[handle].on_response);
+			}
+		}
+	}
 };
