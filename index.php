3900f86b451e9b3dc8085af3a79ca552f5f8b88d
Patrick Lef <patrick@hamsterpaj.net>
2008-08-21 19:03:02 +0200

    Changed number of recent_updates to 11 on frontpage For testing purpose it was changed to 50. Now i changed it back
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
 		$output .= photos_list($photos);
 	}
 	$short_months = array('Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec');	
-	$events = query_cache(array('query' => 'SELECT * FROM recent_updates WHERE label NOT LIKE "Generalrepetition" ORDER BY id DESC LIMIT 50'));
+	$events = query_cache(array('query' => 'SELECT * FROM recent_updates WHERE label NOT LIKE "Generalrepetition" ORDER BY id DESC LIMIT 11'));
 
 	$output .= '<ol id="fp_event_list">' . "\n";
 	foreach($events AS $event)
