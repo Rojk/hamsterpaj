709ce7b0f443a41e870a419f8ef0084e3d3e7dfc
Patrick Lef <patrick@hamsterpaj.net>
2008-08-23 13:46:50 +0200

    Made friends.lib.php friends_fetch return the friends onlinestatus
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/libraries/friends.lib.php
+++ include/libraries/friends.lib.php
@@ -6,8 +6,13 @@
 		$query .= ' WHERE f.user_id = "' . $options['user_id'] . '" AND l.id = f.friend_id AND u.userid = l.id AND is_removed = 0';
 		$query .= ' ORDER BY l.username ASC';
 		$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);	
-		$data = mysql_fetch_assoc($result);
+		$friends = mysql_fetch_assoc($result);
 		
-		return $return;
+			foreach($friends AS $friend)
+			{
+				$friends['onlinestatus'] = login_onlinestatus($friends['lastaction'], $friends['lastrealaction']);
+			}
+		
+		return $friends;
 	}
 ?>
