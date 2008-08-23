f8f4c166e0425a7e92e0d2a6c7b1d1a244c50f7f
Patrick Lef <patrick@hamsterpaj.net>
2008-08-23 20:27:33 +0200

    Made friends_online module shorten friends user status.
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/ui_module/friends_online.module.php
+++ include/ui_module/friends_online.module.php
@@ -7,7 +7,8 @@
 		$friend_onlinestatus = login_onlinestatus($friend['lastaction'], $friend['lastaction']);
 		if($friend_onlinestatus['handle'] == 'online')
 		{
-			$options['output'] .= '						<li><a class="ui_business_card" href="#"><img src="http://images.hamsterpaj.net/famfamfam_icons/status_online.png" /></a> <a href="/traffa/profile.php?user_id=' . $friend['user_id'] . '">' . $friend['username'] . '</a> - ' . ((strlen(trim($friend['user_status'])) > 0) ? $friend['user_status'] : 'Ingen status') . '</li>' . "\n";
+			$friend_status = (mb_strlen($friend['user_status'], 'UTF8') > 17) ? mb_substr($friend['user_status'], 0, 14, 'UTF8') . '...' : $friend['user_status'];
+			$options['output'] .= '						<li><a class="ui_business_card" href="#"><img src="http://images.hamsterpaj.net/famfamfam_icons/status_online.png" /></a> <a href="/traffa/profile.php?user_id=' . $friend['user_id'] . '">' . $friend['username'] . '</a> - ' . ((strlen(trim($friend['user_status'])) > 0) ? $friend_status : 'Ingen status') . '</li>' . "\n";
 		}
 	}
 	$options['output'] .= '					</ul>' . "\n";
