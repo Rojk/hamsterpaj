dc3f877dbc06aaba786a2d55cfa9eee0dcc9c319
Patrick Lef <patrick@hamsterpaj.net>
2008-08-23 16:41:48 +0200

    Made friends.lib.php/friends_fetch getting the friends session_id
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/libraries/friends.lib.php
+++ include/libraries/friends.lib.php
@@ -1,7 +1,7 @@
 <?php
 	function friends_fetch($options)
 	{
-		$query = 'SELECT f.friend_id AS user_id, l.username, l.lastaction, l.lastrealaction, u.user_status, u.image, u.gender, u.birthday';
+		$query = 'SELECT f.friend_id AS user_id, l.username, l.lastaction, l.lastrealaction, l.session_id, u.user_status, u.image, u.gender, u.birthday';
 		$query .= ' FROM friendslist AS f, login AS l, userinfo AS u';
 		$query .= ' WHERE f.user_id = "' . $options['user_id'] . '" AND l.id = f.friend_id AND u.userid = l.id AND is_removed = 0';
 		$query .= ' ORDER BY l.username ASC';
