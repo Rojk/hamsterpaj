bed4556fbfdce3834d95cdee255485900b37ff2e
Patrick Lef <patrick@hamsterpaj.net>
2008-08-23 16:44:28 +0200

    Made ui_module-friends_online dynamic. It fetch friends by using friends.lib.php/fetch_friends and then filter out those who are online
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/ui_module/friends_online.module.php
+++ include/ui_module/friends_online.module.php
@@ -1,8 +1,14 @@
 <?php
+	$friends = $_SESSION['friends'];
+
 	$options['output'] .= '					<ul>' . "\n";
-	$options['output'] .= '						<li><a class="ui_business_card" href="#"><img src="http://images.hamsterpaj.net/famfamfam_icons/status_online.png" /></a> <a href="#">Johan</a> - Nya grejer på g!</li>' . "\n";
-	$options['output'] .= '						<li><a class="ui_business_card" href="#"><img src="http://images.hamsterpaj.net/famfamfam_icons/status_online.png" /></a> <a href="#">Heggan</a> - Myser med Ida <3</li>' . "\n";
-	$options['output'] .= '						<li><a class="ui_business_card" href="#"><img src="http://images.hamsterpaj.net/famfamfam_icons/status_away.png" /></a> <a href="#" class="ui_business_card">Soode</a> - En github</li>' . "\n";
+	foreach($friends as $friend)
+	{
+		if($friend['onlinestatus']['handle'] == 'online')
+		{
+			$options['output'] .= '						<li><a class="ui_business_card" href="#"><img src="http://images.hamsterpaj.net/famfamfam_icons/status_online.png" /></a> <a href="/traffa/profile.php?user_id=' . $friend['user_id'] . '">' . $friend['username'] . '</a> - ' . ((strlen(trim($friend['user_status'])) > 0) ? $friend['user_status'] : 'Ingen status') . '</li>' . "\n";
+		}
+	}
 	$options['output'] .= '					</ul>' . "\n";
 	$options['output'] .= '					<p><a href="/traffa/friends.php">Visa alla vänner &raquo;</a></p>' . "\n";
 ?>
