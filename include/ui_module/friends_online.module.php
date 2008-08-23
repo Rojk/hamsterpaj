b5c6f1e178fe8557bdae2d548a41a89ff20dc3b8
Patrick Lef <patrick@hamsterpaj.net>
2008-08-23 20:09:37 +0200

    Made friends online module check if friend is online by it self. That way it gets better control of the friends status.
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/ui_module/friends_online.module.php
+++ include/ui_module/friends_online.module.php
@@ -4,7 +4,8 @@
 	$options['output'] .= '					<ul>' . "\n";
 	foreach($friends as $friend)
 	{
-		if($friend['onlinestatus']['handle'] == 'online')
+		$friend_onlinestatus = login_onlinestatus($friend['lastaction'], $friend['lastaction']);
+		if($friend_onlinestatus['handle'] == 'online')
 		{
 			$options['output'] .= '						<li><a class="ui_business_card" href="#"><img src="http://images.hamsterpaj.net/famfamfam_icons/status_online.png" /></a> <a href="/traffa/profile.php?user_id=' . $friend['user_id'] . '">' . $friend['username'] . '</a> - ' . ((strlen(trim($friend['user_status'])) > 0) ? $friend['user_status'] : 'Ingen status') . '</li>' . "\n";
 		}
