367da24defc6665376f05f6cf63bb5e1e03c4f7c
Patrick Lef <patrick@hamsterpaj.net>
2008-08-24 17:57:28 +0200

    Added function for smart fetching of online friends The function is server effective att shouldn't fetch more data than is needed
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/libraries/friends.lib.php
+++ include/libraries/friends.lib.php
@@ -14,4 +14,20 @@
 		
 		return $friends;
 	}
+	
+	function friends_fetch_online_smart($options)
+	{
+		$query = 'SELECT f.friend_id AS user_id, l.username, l.lastaction, l.lastrealaction, u.user_status';
+		$query .= ' FROM friendslist AS f, login AS l, userinfo AS u';
+		$query .= ' WHERE f.user_id = "' . $options['user_id'] . '" AND l.id = f.friend_id AND u.userid = l.id AND is_removed = 0 AND l.lastaction >= "' . (time()-600) . '"';
+		$query .= ' ORDER BY l.username ASC';
+		jscript_alert($query);
+		$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);	
+		while($data = mysql_fetch_assoc($result))
+		{
+			$friends[$data['user_id']] = $data; // Save in array
+		}
+		
+		return $friends;
+	}
 ?> 
