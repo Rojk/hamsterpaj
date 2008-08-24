196f7aad98ed916c672e6a8506b329b409a06a48
Erik Andersson <erik.rothoff@gmail.com>
2008-08-24 15:20:24 +0200

    Added JSON-support to the business card Information about the user is fetched in JSON-format from ajax_gateways/business_card.json.php and added to the card. The user can now also send guestbook posts via the business card.
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- javascripts/ui_business_card.js
+++ javascripts/ui_business_card.js
@@ -1,4 +1,7 @@
+var bc_active_id = 0;
+
 jQuery.fn.extend({
+	// traverses through a node-list to check (and return) an element based on the selectors passed in
  	isHas: function(selector) {
 		var ret = false;
 		var chk = $(selector);
@@ -15,21 +18,15 @@ jQuery.fn.extend({
 	},
 	
 	business_card: function() {
+		// create the HTML
 	 	var html = '<div id="ui_business_card">' +
 	 					'<a id="ui_business_card_close" href="#">x</a>' +
-		 				'<a href="/foo/Erkie"><img class="ui_avatar" src="http://images.hamsterpaj.net/images/users/thumb/557316.jpg" alt="" /></a>' +
-		 				'<h1><img src="http://images.hamsterpaj.net/famfamfam_icons/status_offline.png" /> <a href="#">Johan</a></h1>' +
+		 				'<a class="ui_business_card_profile_link" href=""><img class="ui_avatar" id="ui_business_card_avatar" src="" alt="" /></a>' +
+		 				'<h1><img id="ui_business_card_online" src="" /> <a href="" class="ui_business_card_profile_link" id="ui_business_card_username"></a></h1>' +
 		 				'<div id="ui_business_card_about">' +
-		 					'<p>P12 fr&aring;n Flen</p>' +
-		 					'<p>Adda min MSN: kat_ turk_ med_ liten_dase @hotmale. com</p>' +
-		 					'<p>' +
-								'<img src="http://images.hamsterpaj.net/user_flags/computer_geek.png" alt="Datanˆrd" title="Datanˆrd" id="30">' +
-								'<img src="http://images.hamsterpaj.net/user_flags/soccer.png" alt="Fotbollsspelare" title="Fotbollsspelare" id="38">' +
-								'<img src="http://images.hamsterpaj.net/user_flags/swimming.png" alt="Simmare" title="Simmare" id="48">' +
-								'<img src="http://images.hamsterpaj.net/user_flags/sweden.png" alt="Mitt land: Sverige" title="Mitt land: Sverige" id="74">' +
-								'<img src="http://images.hamsterpaj.net/user_flags/teachers_pet.png" alt="Pluggh‰st" title="Pluggh‰st" id="78">' +
-								'<img src="http://images.hamsterpaj.net/user_flags/gamer.png" alt="Gamer" title="Gamer" id="83">' +
-							'</p>' +
+		 					'<p id="ui_business_card_userinfo"></p>' +
+		 					'<p id="ui_business_card_status"></p>' +
+		 					'<p id="ui_business_card_flags"></p>' +
 		 				'</div>' +
 		 				'<div id="ui_business_card_guestbook">' +
 		 					'<form action="#" method="post">' +
@@ -38,30 +35,64 @@ jQuery.fn.extend({
 		 					'</form>' +
 		 				'</div>' +
 		 			'</div>';
+		
+		// save the elements for later use (saves us a bit of time)
 		var card = $(html).hide().appendTo(document.body);
 		var close = $('#ui_business_card_close');
-		var avatar = $('#ui_business_card_avatar');
 		
+		var avatar = $('#ui_business_card_avatar');
+		var profile_links = $('.ui_business_card_profile_link');
+		var userinfo = $('#ui_business_card_userinfo');
+		var status = $('#ui_business_card_status');
+		var flags = $('#ui_business_card_flags');
+		var username = $('#ui_business_card_username');
+		var online = $('#ui_business_card_online');
+			
+		// close
 		close.click(function() {
 		 	card.fadeOut();
 			return false;
 		});
 		
+		// dcocument.click
 		this.click(function(e) {
 			var target = $(e.target);
 			if ( target.isHas('.ui_business_card') === true ) {
 				// we caught it!
-				
-				var w = $(window);
-				card.css({
-					'left': ($('#ui_wrapper').width() / 2) - (card.width() / 2),
-				 	'top': w.scrollTop() + (w.height() / 2) - (card.height() / 2)
-				}).fadeIn(function() {
-					// center avatar
-					var height = card.height() - parseInt(card.css('padding-top'), 10) - parseInt(card.css('padding-left'), 10);
-					avatar.height(height);
+				var href = target.attr('href');
+				var user_id = 65654;
+				href.replace(/id=([0-9]+)/, function() {
+					user_id = arguments[1];
 				});
 				
+				var w = $(window);			
+				$.getJSON('/ajax_gateways/business_card.json.php?user_id=' + user_id, function(data) {
+					bc_active_id = data.user_id;
+			
+					profile_links.attr('href', '/traffa/profile.php?user_id=' + data.user_id);
+					avatar.attr('src', 'http://images.hamsterpaj.net/images/users/thumb/' + data.user_id + '.jpg');
+					userinfo.html(data.user_info);
+					status.html(data.status);
+					flags.html(bc_make_flags(data.flags));
+					username.html(data.username);
+					
+					// online
+					if ( data.online == true ) {
+						 online.attr('src', 'http://images.hamsterpaj.net/famfamfam_icons/status_online.png');
+						 online.attr('title', 'Online');
+					// offline
+					} else {
+						online.attr('src', 'http://images.hamsterpaj.net/famfamfam_icons/status_offline.png');
+						online.attr('title', 'Offline');
+					}
+					
+					card.css({
+						'left': ($('#ui_wrapper').width() / 2) - (card.width() / 2),
+						'top': w.scrollTop() + (w.height() / 2) - (card.height() / 2)
+					}).fadeIn();
+				});
+				
+				// close
 				var d = $(document);
 				d.bind('click.cardclose', function(ev) {
 				 	var targ = $(ev.target);
@@ -74,8 +105,44 @@ jQuery.fn.extend({
 				return false;
 			}
 		});
+		
+		// guestbook_form
+		var txtarea = $('#ui_business_card_guestbook textarea');
+		var submitbtn = $('#ui_business_card_guestbook input[type=submit]');
+		var form = $('#ui_business_card_guestbook form').submit(function(e) {
+			submitbtn.val('Sparar...').attr('disabled', 'true');
+			var data = {
+				action: 'insert',
+				recipient: bc_active_id,
+				message: txtarea.val(),
+				is_private: false
+			};
+			$.post('/ajax_gateways/guestbook.json.php', data, function() {
+				submitbtn.val('Sparat');
+				card.fadeOut();
+				setTimeout(function() {
+					txtarea.val('');
+					submitbtn.val('Skicka g√§stboksinl√§gg').attr('disabled', false);
+				}, 5000);
+			});
+			return false;
+		});
+		
+		// avatar
+		avatar.click(function() {
+			window.open('/avatar.php?id=' + bc_active_id, 'avatar_window' + bc_active_id, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=410, height=600')
+			return false;
+		});
 	}
 });
+
+function bc_make_flags(flags) {
+	var html = '';
+	$.each(flags, function(i, item) {
+		html += '<img src="' + item + '" alt="' + i + '" title="' + i + '" />';
+	});
+	return html;
+}
 
 $(document).ready(function() {
 	$(document).business_card();
