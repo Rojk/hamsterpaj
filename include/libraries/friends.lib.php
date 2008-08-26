47d68cd421e9726427adcd23d4818887f4b2bb01
Patrick Lef <patrick@hamsterpaj.net>
2008-08-26 16:31:03 +0200

    Made a better check of which friends who are online for module Before it was enough to have a window of HP open to bee set as online. Now we check with lastrealaction to. That will hopefully give us a more exact result
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/libraries/friends.lib.php
+++ include/libraries/friends.lib.php
@@ -18,7 +18,7 @@
 	{
 		$query = 'SELECT f.friend_id AS user_id, l.username, l.lastaction, l.lastrealaction, u.user_status';
 		$query .= ' FROM friendslist AS f, login AS l, userinfo AS u';
-		$query .= ' WHERE f.user_id = "' . $options['user_id'] . '" AND l.id = f.friend_id AND u.userid = l.id AND is_removed = 0 AND l.lastaction >= "' . (time()-180) . '"';
+		$query .= ' WHERE f.user_id = "' . $options['user_id'] . '" AND l.id = f.friend_id AND u.userid = l.id AND is_removed = 0 AND l.lastaction >= "' . (time()-120) . '" AND l.lastrealaction >= "' . (time()-300) . '"';
 		$query .= ' ORDER BY l.username ASC';
 		$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);	
 		while($data = mysql_fetch_assoc($result))
