5ef99d02f952bfab1db03edb635d5c5c59897eaa
Patrick Lef <patrick@hamsterpaj.net>
2008-09-03 15:17:19 +0200

    Minor fix
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/ui_module/friends_online.module.php
+++ include/ui_module/friends_online.module.php
@@ -14,7 +14,7 @@
 	foreach($friends as $friend)
 	{
 			$friend_status = (mb_strlen($friend['user_status'], 'UTF8') > 17) ? mb_substr($friend['user_status'], 0, 14, 'UTF8') . '...' : $friend['user_status'];
-			$options['output'] .= '						<li><a class="ui_business_card" href="/traffa/profile.php?user_id=' . $friend['user_id'] . '"><img src="http://images.hamsterpaj.net/famfamfam_icons/status_online.png" /></a> <a href="/traffa/profile.php?user_id=' . $friend['user_id'] . '">' . $friend['username'] . '</a> - ' . ((strlen(trim($friend['user_status'])) > 0) ? $friend_status : 'Ingen status') . '</li>' . "\n";
+			$options['output'] .= '						<li><a class="ui_business_card" href="/traffa/profile.php?user_id=' . $friend['user_id'] . '"><img src="http://images.hamsterpaj.net/famfamfam_icons/status_online.png" /></a> <a href="/traffa/profile.php?user_id=' . $friend['user_id'] . '">' . $friend['username'] . '</a> - <span title="' . ((strlen(trim($friend['user_status'])) > 0) ? $friend['user_status'] : 'Ingen status') . '">' . ((strlen(trim($friend['user_status'])) > 0) ? $friend_status : 'Ingen status') . '</span></li>' . "\n";
 	}
 	$options['output'] .= '					</ul>' . "\n";
 	$options['output'] .= '					<p><a href="/traffa/friends.php">Visa alla vänner &raquo;</a></p>' . "\n";
