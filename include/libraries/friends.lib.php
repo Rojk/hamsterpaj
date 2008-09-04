59aa9a37efa90d926ff9e6a95b270c2e3d5b8382
Patrick Lef <patrick@hamsterpaj.net>
2008-09-04 19:54:14 +0200

    Added functions for friends actions
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/libraries/friends.lib.php
+++ include/libraries/friends.lib.php
@@ -28,4 +28,36 @@
 		
 		return $friends;
 	}
+	
+	function friends_actions_fetch($options)
+	{
+		$query = 'SELECT f.friend_id AS user_id, f.url, f.action_id, f.read, f.action, f.label, l.username';
+		$query .= ' FROM friends_notices AS f, login AS l, userinfo AS u';
+		$query .= ' WHERE f.user_id = "' . $options['user_id'] . '"';
+		$query .= ' AND l.id = f.friend_id';
+		$query .= ' AND u.userid = l.id';
+		$query .= ' AND l.is_removed = 0';
+		$query .= $options['show'] == "new" ? ' AND f.read = "0"' : '';
+		$query .= ' ORDER BY l.username ASC';
+		$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);	
+		while($data = mysql_fetch_assoc($result))
+		{
+			$friends_notices[$data['user_id']]['actions'][$data['action_id']] = $data; // Save in array
+			$friends_notices[$data['user_id']]['username'] = $data['username'];
+		}
+		jscript_alert($query);
+		return $friends_notices;
+	}
+	
+	function friends_actions_insert($options)
+	{
+		$friends_options['user_id'] = $_SESSION['login']['id'];
+		$friends = friends_fetch($friends_options);
+		foreach($friends as $friend)
+		{
+			$query = 'INSERT INTO friends_notices (user_id, timestamp, friend_id, action, url, label)';
+			$query .= ' VALUES("' . $friend['user_id'] . '", "' . time() . '", "' . $_SESSION['login']['id'] . '", "' . $options['action'] . '", "' . $options['url'] . '", "' . $options['label'] . '")';
+			$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);
+		}
+	}
 ?> 
