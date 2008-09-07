caac2594d4e9614e6cad9e876f20bc158db90648
Joel <joel@hamsterpaj.net>
2008-09-07 13:47:03 +0200

    Forgot to put quotes around forum thread-title in synchronize for notices. Fixed now :)
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- ajax_gateways/synchronize.json.php
+++ ajax_gateways/synchronize.json.php
@@ -29,7 +29,7 @@
 					foreach($_SESSION['forum']['subscriptions'] as $subscription)
 					{
 						$url = (isset($thread['url'])) ? $thread['url'] : $thread['handle'] . '/sida_1.php';
-						$subscriptions[] = '{"title": ' . addslashes($subscription['title']) . ', "url": "' . urlencode($url) . '", "unread_posts": ' . $subscription['unread_posts'] . '}';
+						$subscriptions[] = '{"title": "' . addslashes($subscription['title']) . '", "url": "' . urlencode($url) . '", "unread_posts": ' . $subscription['unread_posts'] . '}';
 					}
 					$data = '{"new_notices": ' . $notices['discussion_forum']['new_notices'] . ', "subscriptions": [' . implode(', ', $subscriptions) . ']}';
 				break;
