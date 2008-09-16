276a6ca2b3eef62d9e60da5baa2b3c1aec7eb23c
Joel <joel@hamsterpaj.net>
2008-09-16 21:34:05 +0200

    Made switch_design.php available for everyone.
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- switch_design.php
+++ switch_design.php
@@ -1,9 +1,5 @@
 <?php
 	include 'include/core/common.php';
-	if(!in_array(strtolower($_SESSION['login']['username']), array('joar', 'heggan', 'joel', 'lef-91', 'anoosmonkey', 'iphone', 'soode', 'rojk', 'johan', 'ace', 'entrero')))
-	{
-		return;
-	}
 
 	if(isset($_SESSION['new_design']))
 	{
