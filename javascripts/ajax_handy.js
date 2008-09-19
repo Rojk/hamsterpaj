dd39f167e2a47d9639dbc83b4fdd662f86af1e1a
Joar Wandborg <joar@hamsterpaj.net>
2008-09-19 18:20:50 +0200

    Fixed Ajax Handy :)
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- javascripts/ajax_handy.js
+++ javascripts/ajax_handy.js
@@ -1,3 +1,16 @@
+$(document).ready(function () {
+	
+	$('#submit_button').click(function () {
+		
+		$.get('/ajax_gateways/ajax_handy.php', { action: $('#send_select').val(), value: $('#send_value').val() },
+			function(data){
+			//alert("Data Loaded: " + data);
+			$('#response_area').text(data);
+		 });
 
+		return false;
+	});
+	
+});
 
 womAdd();
