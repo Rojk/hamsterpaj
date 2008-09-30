e7aa81d030bb6f972b7ec5c949de4e79dec636ff
Joar Wandborg <joar@hamsterpaj.net>
2008-09-30 23:51:48 +0200

    Third try...
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- traffa/age_guess_image.php
+++ traffa/age_guess_image.php
@@ -6,10 +6,10 @@
 	
 	$sql = 'SELECT id FROM login WHERE MD5(id) = ' . $_GET['user_hash'];
 	
-	$result = mysql_query($sql);
+	$result = mysql_query($sql) or die('Dummer, det här fungerar ju inte.');
 	
 	$data = mysql_fetch_assoc($result);
 	
-	$out = readfile(IMAGE_PATH . 'images/users/full/' . $data['id'] . '.jpg') or die('FISKROJK! BOM-SPÖKENA JAGAR MIG');
+	$out = readfile(IMAGE_PATH . 'images/users/full/' . $data['id'] . '.jpg') or die('FISKROJK! BOM-SPÖKENA JAGAR MIG') && preint_r($data);
 	
 	echo $out;
