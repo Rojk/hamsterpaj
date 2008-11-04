9064ce7b2fd247eb2ef1778b498d946a41c8a681
Joar Wandborg <joar@hamsterpaj.net>
2008-11-04 20:19:10 +0100

    Bella takes her first steps <3
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

---
+++ fotoblogg/index.php
@@ -0,0 +1,49 @@
+<?php
+	require('../include/core/common.php');
+	
+	$uri_parts = explode('/', $_SERVER['REQUEST_URI']);
+	
+	$out .= '<ul>
+	<li>
+		<a href="/fotoblogg/">Min dagbok</a>
+	</li>
+	<li>
+		<a href="/fotoblogg/Lef/">Lefs dagbok</a>
+	</li>
+	<li>
+		<a href="/fotoblogg/ladda_upp/">Ladda upp</a>
+	</li>
+	<li>
+		<a href="/fotoblogg/instaellningar/">Inställningar</a>
+	</li>
+	</ul>
+	
+	' . "\n";
+	
+	switch ($uri_parts[2])
+	{
+		case 'instaellningar':
+			$out .= '<h2>INSTÄLLNINGAR</h2>' . "\n";
+		break;
+		
+		case 'ladda_upp':
+			$out .= '<h2>LADDA UPP!</h2>' . "\n";
+		break;
+			
+		default:
+			$out .= 'Välkommen till ' . "\n";
+			$out .= preg_match('/s$/', $uri_parts[2]) ? $uri_parts[2] : $uri_parts[2] . 's';
+			$out .= ' fotoblogg!';
+		break;
+	}
+	$out .= '<br /><br />' . preint_r($uri_parts);
+	/*
+	/fotoblogg/user/
+	/fotoblogg/instaellningar
+	/fotoblogg/ladda_upp
+	/fotoblogg <- sin egen
+	*/
+	ui_top($ui_options);
+	echo $out;
+	ui_bottom();
+?>
