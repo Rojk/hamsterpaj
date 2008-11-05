1f7bbbf21859d1b9ccd4b82e7e1f97b1bb0f2fce
Patrick Lef <patrick@hamsterpaj.net>
2008-11-05 17:02:20 +0100

    Added some styling for photoblog upload
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- fotoblogg/index.php
+++ fotoblogg/index.php
@@ -2,6 +2,8 @@
 	require('../include/core/common.php');
 	require(PATHS_INCLUDE . 'libraries/photoblog.lib.php');
 	
+	$ui_options['stylesheets'][] = 'photoblog.css.php';
+	
 	$uri_parts = explode('/', $_SERVER['REQUEST_URI']);
 	
 	$out .= '<ul>
@@ -28,7 +30,71 @@
 		break;
 		
 		case 'ladda_upp':
-			$out .= '<h2>LADDA UPP!</h2>' . "\n";
+			/*	
+				########################################################
+					Page title
+				########################################################
+			*/
+			$out .= '<h1>Välkommen att ladda upp bilder i din fotoblogg</h1>' . "\n";
+			
+			/*	
+				########################################################
+					Upload form
+				########################################################
+			*/
+			$out .= '<object width="100" height="40">
+							 <param name="movie" value="../upload.swf">
+							 <embed src="../upload.swf" width="100" height="40"></embed>
+								</object>' . "\n";  
+								
+			/*	
+				########################################################
+					Uploaded photos setting
+				########################################################
+			*/
+			$out .= '<div class="photoblog_photo_properties" id="#">' . "\n";
+				$out .= '<div class="properties">' . "\n";
+					$out .= '<p>Datepicker - save - set today | Select album - Create album</p>';
+					$out .= '<p>WYSIWYG-editor tinymce</p>';
+					$out .= '<p>Save</p>';
+				$out .= '</div>' . "\n";
+				$out .= '<div class="float">' . "\n";
+					$out .= '<div class="thumbnail_wrapper">' . "\n";
+						$out .= '<img src="http://images.hamsterpaj.net/photos/thumb/8/42818.jpg" class="thumbnail" />' . "\n";
+					$out .= '</div>' . "\n";
+					$out .= '<div class="rotate">' . "\n";
+						$out .= '<img src="" class="rotate_left" />' . "\n";
+						$out .= '<img src="" class="rotate_right" />' . "\n";
+					$out .= '</div>' . "\n";
+				$out .= '</div>' . "\n";
+			$out .= '</div>' . "\n";
+								
+			/*	
+				########################################################
+					How to upload photos
+				########################################################
+			*/			   
+    	$options['type'] = 'notification';
+    	$options['title'] = '';
+    	$options['message'] = '<p>Du kan ladda upp flera bilder samtidigt genom att markera flera när du väljer bilder.<br /> Men tänk på att det tar en del tid att ladda upp bilderna, och du bör kanske inte ladda upp så många i taget.</p><p>Fungerar inte bilduppladdningen? Klicka <a href="#">här</a> för att använda en enklare version av uppladdningen</a>';
+    	$out .= ui_server_message($options);   
+    	
+    	/*	
+				########################################################
+					Rules of what to upload
+				########################################################
+			*/
+			$options['type'] = 'warning';
+			$options['title'] = 'Att tänka på innan du laddar upp bilder';
+			$options['message'] = '<h3>Du förlorar kontrollen över bilder du laddar upp!</h3>' . "\n";
+			$options['message'] .= '<p>Bilder som en gång laddats upp till Internet kan kopieras och skickas vidare i all evighet. Det gäller på Hamsterpaj såväl som på alla andra webbsajter.</p>' . "\n";
+			$options['message'] .= '<h3>Är du en blond tjej med stora tuttar eller kille med brunt hår och slingor? </h3>' . "\n";
+			$options['message'] .= '<p>Den där blyge typen med fula glasögon och som luktade äckligt i din klass i mellanstadiet kommer förr eller senare stjäla din bild för att ragga på Lunarstorm, Hamsterpaj, PlayAhead och andra communities. När det händer så kontaktar du en ordningsvakt så löser vi det!</p>' . "\n";
+			$options['message'] .= '<h3>Hamsterpaj är ingen porrsajt, Goatse är äckligt och hitlerhälsningar olagliga</h3>' . "\n";
+			$options['message'] .= '<p>Snälla låt bli porr och goatse här, tänk på att barn besöker den här sajten!</p>' . "\n";
+			$options['message'] .= '<em>Brottsbalkens sextonde kapitel, paragraf åtta</em><br />' . "\n";
+			$options['message'] .= '<p>8 § Den som i uttalande eller i annat meddelande som sprids hotar eller uttrycker missaktning för folkgrupp eller annan sådan grupp av personer med anspelning på ras, hudfärg, nationellt eller etniskt ursprung, trosbekännelse eller sexuell läggning, döms för hets mot folkgrupp till fängelse i högst två år eller om brottet är ringa, till böter.</p>' . "\n";
+			$out .= ui_server_message($options);
 		break;
 			
 		default:
---
+++ stylesheets/photoblog.css.php
@@ -0,0 +1,31 @@
+.photoblog_photo_properties {
+	border: 1px solid #cfe2f1;
+	-moz-border-radius: 5px;
+	background: #f1f5ff;
+}
+	.photoblog_photo_properties .float {
+		width: 176px;
+	}
+		.photoblog_photo_properties .thumbnail_wrapper {
+			padding: 1px;
+			border: 2px solid;
+			width: 150px;
+			margin: 10px;
+		}
+		.photoblog_photo_properties .rotate {
+			margin: 0 50px 10px 50px;
+			width: 176px;
+		}
+			.photoblog_photo_properties .rotate_left {
+				margin-right: 10px;
+				display: inline;
+			}
+			.photoblog_photo_properties .rotate_right {
+				display: inline;
+			}
+	.photoblog_photo_properties .properties {
+		width: 206px;
+		margin-left: 166px;
+		padding: 10px;
+		position: absolute;
+	}
