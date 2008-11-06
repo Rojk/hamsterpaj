d5f2a015817432097f29f3b1419cb63696caa20e
Joel <joel@hamsterpaj.net>
2008-11-06 17:34:52 +0100

    Moved session re-assignment directive to a location before parsing common.php.
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- fotoblogg/upload.php
+++ fotoblogg/upload.php
@@ -1,12 +1,12 @@
 <?php
-	require('../include/core/common.php');
-	require(PATHS_INCLUDE . 'libraries/photoblog.lib.php');
-	
 	if(isset($_GET['PHPSESSID']))
 	{
 		session_id($_GET['PHPSESSID']);
-	}
+	}	
 	
+	require('../include/core/common.php');
+	require(PATHS_INCLUDE . 'libraries/photoblog.lib.php');
+		
 	try
 	{
 		if(is_uploaded_file($_FILES['Filedata']['tmp_name']))
