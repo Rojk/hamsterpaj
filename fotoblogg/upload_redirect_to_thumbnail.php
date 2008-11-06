5fdfbfa9e0395ad9377ba8c7bfc21667442a51d8
Joel <joel@hamsterpaj.net>
2008-11-06 23:24:01 +0100

    Minor fix
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- fotoblogg/upload_redirect_to_thumbnail.php
+++ fotoblogg/upload_redirect_to_thumbnail.php
@@ -1,7 +1,7 @@
 <?php
 	require('../include/core/common.php');
 	
-	if(login_checklogin() && isset($_GET['upload_ticket']) && isset($_GET['photo_id']) && is_numeric($_GET['upload_ticket']) && is_numeric($_GET['photo_id']))
+	if(login_checklogin() && isset($_GET['upload_ticket']) && isset($_GET['photo_id']) && $_GET['upload_ticket'] && is_numeric($_GET['photo_id']))
 	{
 		if(isset($_SESSION['photoblog']['upload']['upload_tickets'][$_GET['upload_ticket']][$_GET['photo_id']]))
 		{
