cde041828691115b69305fc8f96fb7f807859ea7
Joar Wandborg <joar@hamsterpaj.net>
2008-11-06 09:14:42 +0100

    Changed FP.
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- index.php
+++ index.php
@@ -63,9 +63,68 @@
 		}
 	}
 	
-	$output .= '<embed wmode="transparent" style="z-index: 1; float: left;" src="http://www.sheeptobechic.com/sheep.swf" width="400" height="246" allowfullscreen="true" />';
+	$output .= '<embed wmode="transparent" style="z-index: 1; float: left;" src="http://www.sheeptobechic.com/sheep.swf" width="317" height="199" allowfullscreen="true" />';
 	
-		$output .= '<a style="float: right;" href="http://www.hamsterpaj.net/onlinespel/crews_favoritspel/puzzle_farter.html"><img src="http://images.hamsterpaj.net/puzzle_farter.png" /></a>' . "\n";
+	// -----------------------
+	// --- SPOTLIGHT START ---
+	// -----------------------
+	//                                     |<---     TEMPORARY STYLE     --->|
+	$output .= '<div id="fp_spotlight_area" style="width: 280px; float:right;">' . "\n";
+		
+	$users = cache_load('hetluften');
+
+	$output .= '<div id="fp_spotlight">' . "\n";
+	$output .= '<div id="fp_spotlight_scroller">' . "\n";
+	foreach($users AS $user)
+	{
+		$output .= '<div class="fp_spotlight_profile">' . "\n";
+		$output .= ui_avatar($user['id'], array('style' => 'float: left; margin-right: 15px; border: 1px solid white;'));
+		$output .= '<h2><a href="/traffa/profile.php?id=' . $user['id'] . '">' . $user['username'] . '</a></h2>' . "\n";
+		$output .= ($user['gender'] == 'f') ? '<p>Tjej' : '<p>Kille';
+		$output .= ($user['birthday'] != '0000-00-00') ? ' ' . date_get_age($user['birthday']) . ' år' : '';
+		$output .= (strlen($user['spot']) > 0) ? ' från ' . $user['spot'] . '</p>' : '</p>';
+		if(count($user['flags']) > 0)
+		{
+			$output .= '<ul class="user_flags">' . "\n";
+			$flag_count = 0;
+			foreach($user['flags'] AS $flag)
+			{
+				if(strlen($flags_by_id[$flag]) > 0)
+				{
+					$output .= '<li><img src="' . IMAGE_URL . '/user_flags/' . $flags_by_id[$flag] . '" /></li>' . "\n";
+					$flag_count++;
+					if($flag_count == 5)
+					{
+						break;
+					}
+				}
+			}
+			$output .= '</ul>' . "\n";
+		}
+		$output .= '</div>' . "\n";
+	}
+	$output .= '</div>' . "\n";
+	$output .= '</div>' . "\n";
+
+
+	$output .= '<ul class="fp_users_list">' . "\n";
+	$count = 0;
+	foreach($users AS $user)
+	{
+		$output .= '<li><img src="' . IMAGE_URL . 'images/users/thumb/' . $user['id'] . '" class="fp_user_list_thumb" id="fp_user_thumb_' . $count . '" /></li>' . "\n";
+		$count++;
+	}
+	$output .= '</ul>' . "\n";
+	
+	
+	$output .= '<p style="clear: both;">Gå till: <a href="/traffa/age_guess.php">Gissa åldern</a> ' . "\n";
+	$output .= '<a href="/traffa/gallery.php">Galleriet</a> ' . "\n";
+	$output .= '<a href="/traffa/klotterplanket.php">klotterplanket</a></p>' . "\n";
+	$output .= '</div>' . "\n";
+	
+	// -----------------------
+	// ---- SPOTLIGHT END ----
+	// -----------------------
 	
 	$output .= '<h1 id="fp_greeting">' . $page_heading . '</h1>' . "\n";
 	
@@ -160,59 +219,7 @@
 		$output .= '</div>' . "\n";
 	}	
 
-	/* Spotlight */
-	$output .= '<div id="fp_spotlight_area">' . "\n";
-		
-	$users = cache_load('hetluften');
-
-	$output .= '<div id="fp_spotlight">' . "\n";
-	$output .= '<div id="fp_spotlight_scroller">' . "\n";
-	foreach($users AS $user)
-	{
-		$output .= '<div class="fp_spotlight_profile">' . "\n";
-		$output .= ui_avatar($user['id'], array('style' => 'float: left; margin-right: 15px; border: 1px solid white;'));
-		$output .= '<h2><a href="/traffa/profile.php?id=' . $user['id'] . '">' . $user['username'] . '</a></h2>' . "\n";
-		$output .= ($user['gender'] == 'f') ? '<p>Tjej' : '<p>Kille';
-		$output .= ($user['birthday'] != '0000-00-00') ? ' ' . date_get_age($user['birthday']) . ' år' : '';
-		$output .= (strlen($user['spot']) > 0) ? ' från ' . $user['spot'] . '</p>' : '</p>';
-		if(count($user['flags']) > 0)
-		{
-			$output .= '<ul class="user_flags">' . "\n";
-			$flag_count = 0;
-			foreach($user['flags'] AS $flag)
-			{
-				if(strlen($flags_by_id[$flag]) > 0)
-				{
-					$output .= '<li><img src="' . IMAGE_URL . '/user_flags/' . $flags_by_id[$flag] . '" /></li>' . "\n";
-					$flag_count++;
-					if($flag_count == 5)
-					{
-						break;
-					}
-				}
-			}
-			$output .= '</ul>' . "\n";
-		}
-		$output .= '</div>' . "\n";
-	}
-	$output .= '</div>' . "\n";
-	$output .= '</div>' . "\n";
-
-
-	$output .= '<ul class="fp_users_list">' . "\n";
-	$count = 0;
-	foreach($users AS $user)
-	{
-		$output .= '<li><img src="' . IMAGE_URL . 'images/users/thumb/' . $user['id'] . '" class="fp_user_list_thumb" id="fp_user_thumb_' . $count . '" /></li>' . "\n";
-		$count++;
-	}
-	$output .= '</ul>' . "\n";
-	
-	
-	$output .= '<p style="clear: both;">Gå till: <a href="/traffa/age_guess.php">Gissa åldern</a> ' . "\n";
-	$output .= '<a href="/traffa/gallery.php">Galleriet</a> ' . "\n";
-	$output .= '<a href="/traffa/klotterplanket.php">klotterplanket</a></p>' . "\n";
-	$output .= '</div>' . "\n";
+	// SPOTLIGHT GOES HERE
 	
 	/* Info text */
 	$output .= '<p>Hamsterpaj är en kul sida med filmer, spel och lite chattfunktioner, till för tonåringar som inte orkar skriva skolarbeten utan slösurfar istället.</p>' . "\n";
