ed61df42033f02ccb6f74bbfecd1e3da64711885
Joar Wandborg <joar@hamsterpaj.net>
2008-11-14 00:19:06 +0100

    Adding priv-check on admin/fp_module_customize.php\!
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- admin/fp_module_customize.php
+++ admin/fp_module_customize.php
@@ -5,6 +5,17 @@
 	$ui_options['title'] = 'Redigera modul';
 	$ui_options['menu_path'] = array('hamsterpaj');
 	$ui_options['adtoma_category'] = 'start';
+	
+	if (!is_privilegied('fp_module_rearrange'))
+	{
+		ui_top($ui_options);
+		echo '<div class="error">';
+		echo '<strong>Nu äter hamstern upp dig! :)</strong>';
+		echo '</div>';
+		ui_bottom();
+		exit;
+	}
+	
 	ui_top($ui_options);
 
 	
