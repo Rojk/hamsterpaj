b5d5dbb0cd842cb5d58c418fd01c2a1a8700a03a
Patrick Lef <patrick@hamsterpaj.net>
2008-11-13 23:48:05 +0100

    Yet another fix, since git ignored my admin control files...

---
+++ admin/fp_module_customize.php
@@ -0,0 +1,53 @@
+<?php
+	require('../include/core/common.php');
+	require(PATHS_INCLUDE  . 'libraries/photos.lib.php');
+	
+	$ui_options['title'] = 'Redigera modul';
+	$ui_options['menu_path'] = array('hamsterpaj');
+	$ui_options['adtoma_category'] = 'start';
+	ui_top($ui_options);
+
+	
+	if(isset($_GET['filename']))
+	{
+		if(isset($_POST['code']))
+		{
+			$module['display'] = ($_POST['display'] == 1) ? 1 : 0;
+			$module['phpenabled'] = ($_POST['phpenabled'] == 1) ? 1 : 0;
+			
+			$module['stylesheets'] = explode(' ', $_POST['stylesheets']);
+			
+			cache_save('fp_module_' . $_GET['filename'], $module);
+			
+			
+			$code = stripslashes(html_entity_decode($_POST['code']));
+			
+			file_put_contents(PATHS_INCLUDE . 'fp_modules/' . $_GET['filename'], $code);
+			
+			$output .= '<h1>Sparat! - <a href="/admin/fp_module_rearrange.php">sortera moduler</a></h1>';
+		}
+		
+		$module = cache_load('fp_module_' . $_GET['filename']);
+		
+		$module['display'] = ($module['display'] == 1) ? ' checked="true"' : '';
+		$module['phpenabled'] = ($module['phpenabled'] == 1) ? ' checked="true"' : '';
+		
+		$module['code'] = file_get_contents(PATHS_INCLUDE . 'fp_modules/' . $_GET['filename']);
+
+		$output .= '<form method="post">' . "\n";
+		$output .= '<input name="display" type="checkbox"' . $module['display'] . ' id="view_control" value="1" /><label for="view_control">Visa på förstasidan</label><br />';
+		$output .= '<input name="phpenabled" type="checkbox"' . $module['phpenabled'] . ' id="php_control" value="1" /><label for="php_control">Kör som PHP-fil (script = kryssa, enkel HTML = kryssa inte)</label><br />';
+		
+		$output .= '<br /><label>CSS-filer (ange bara filnamn, inklusive filändelse, separera med mellanslag)</label><br />' . "\n";
+		$output .= '<input type="text" name="stylesheets" style="width: 600px;" value="' . implode(' ', $module['stylesheets']) . '" /><br />';
+		
+		$output .= '<textarea name="code" style="width: 630px; height: 500px; font-size: 11px;">' . htmlspecialchars($module['code']) . '</textarea>' . "\n";
+
+		$output .= '<input type="submit" value="Spara" />' . "\n";
+		$output .= '</form>' . "\n";
+	}
+
+	echo $output;
+
+	ui_bottom();
+	?>
---
+++ admin/fp_module_rearrange.php
@@ -0,0 +1,46 @@
+<?php
+	require('../include/core/common.php');
+	require(PATHS_INCLUDE  . 'libraries/photos.lib.php');
+
+	$ui_options['javascripts'][] = 'fp_module_rearrange.js';
+	$ui_options['stylesheets'][] = 'fp_module_rearrange.css';
+		
+	
+	$ui_options['title'] = 'Sortera förstasidesmoduler';
+	$ui_options['menu_path'] = array('hamsterpaj');
+	$ui_options['adtoma_category'] = 'start';
+	ui_top($ui_options);
+
+
+	$filenames = cache_load('fp_module_order');
+
+	$dir = opendir(PATHS_INCLUDE . 'fp_modules/');
+	while($filename = readdir($dir))
+	{
+		if($filename != '.' && $filename != '..' && !in_array($filename, $filenames))
+		{
+			$filenames[] = $filename;
+		}
+	}
+
+
+	$output .= '<ul id="fp_module_rearrange_list">';
+	foreach($filenames AS $filename)
+	{
+		$output .= '<li id="' . $filename . '">' . "\n";
+		$output .= '<h3>' . $filename . '</h3>' . "\n";
+		$output .= '<a href="/admin/fp_module_customize.php?filename=' . $filename . '">Redigera</a>' . "\n";
+		$output .= '</li>' . "\n";
+	}
+	$output .= '</ul>';
+
+
+	$output .= '<button id="fp_moudle_rearrange_save">Spara</button>' . "\n";
+	$output .= '<h1>Sparningen kan ta tid</h1>' . "\n";
+	$output .= '<p>Du får inga timglas och ingen indikation på att systemet sparar, du får inte heller veta när det sparat klart. Lämnar du sidan innan sparningen är klar så avrbyts det hela. Kolla alltså att ändringarna vekrligen slår igenom på förstasidan!</p>';
+	echo $output;
+
+
+
+	ui_bottom();
+	?>
