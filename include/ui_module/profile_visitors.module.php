ea0ea0bb874896688027c329b1af907f15b4014d
Patrick Lef <patrick@hamsterpaj.net>
2008-11-15 12:26:32 +0100

    Fixed failed utf-8 encoding on profile_visitors module
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/ui_module/profile_visitors.module.php
+++ include/ui_module/profile_visitors.module.php
@@ -3,7 +3,7 @@
 	foreach($_SESSION['visitors_with_image'] AS $visitor)
 	{
 		$options['output'] .= '<li>' . "\n";
-		$options['output'] .= '<a href="/traffa/profile.php?id=' . $visitor['id'] . '" title="' . $visitor['username'] . ' besökte dig ' . strtolower(fix_time($visitor['timestamp'])) . '">';
+		$options['output'] .= '<a href="/traffa/profile.php?id=' . $visitor['id'] . '" title="' . $visitor['username'] . ' besÃ¶kte dig ' . strtolower(fix_time($visitor['timestamp'])) . '">';
 		$options['output'] .= '<img src="http://images.hamsterpaj.net/images/users/thumb/' . $visitor['id'] . '.jpg" />';
 		$options['output'] .= '</a>' . "\n";
 		$options['output'] .= '</li>' . "\n";
