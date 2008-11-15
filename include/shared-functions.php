a0a6ae468b350d812c8009f762726fa0b4afd5c8
Joar Wandborg <joar@hamsterpaj.net>
2008-11-15 21:21:27 +0100

    Fixed link regex
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- include/shared-functions.php
+++ include/shared-functions.php
@@ -693,7 +693,7 @@ function clickable_links($str)
 	$str = preg_replace('#\[img](.*?)\[/img]#is', '[IMAGE]', $str);
 	
 	// Leta upp länkar
-	$str = preg_replace('#((http://|https://|ftp://|www\.)(www\.)?)([a-z0-9åäöÅÄÖ$\#_%\?&//=\+@\.:;-~()]{4,})#eis', 
+	$str = preg_replace('#((http://|https://|ftp://|www\.)(www\.)?)([a-z0-9åäöÅÄÖ$\#_%\?&//=\+@\.:;-~\(\)]{4,})#eis', 
 						"
 						('$2' != 'http://' && '$2' != 'https://' && '$2' != 'ftp://') ? 
 							'<a href=\"http://$1$4\" target=\"_blank\">' . 
