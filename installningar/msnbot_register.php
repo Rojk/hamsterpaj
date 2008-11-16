cbec2a53caf58ccd6580ba9fa400e42b579da069
Joel <joel@hamsterpaj.net>
2008-11-16 20:46:44 +0100

    Added full support for msnbot_register. userinfo.msnbot_msn = varchar 60
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- installningar/msnbot_register.php
+++ installningar/msnbot_register.php
@@ -6,10 +6,19 @@
 	$ui_options['menu_path'] = array('installningar', 'msnbot');
 	
 	ui_top($ui_options);
+	
+	echo '<h1>Vännernotiser på MSN</h1>';
+	
 	if(login_checklogin())
 	{
 		if(isset($_GET['msn'], $_GET['salt']) && msnbot_is_valid_msn($_GET['msn']) && msnbot_is_valid_salt(array('salt' => $_GET['salt'], 'msn' => $_GET['msn'])))
 		{
+			$query = 'UPDATE userinfo SET msnbot_msn = "'. $_GET['msn'] . '" WHERE userid = ' . $_SESSION['login']['id'];
+			$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);
+			$_SESSION['userinfo']['msnbot_msn'] = $_GET['msn'];
+			
+			echo 'Du är nu registrerad!';
+			/*
 			$_SESSION['msnbot']['register_msn'] = $_GET['msn'];
 			echo 'Välj vad du vill få ett meddelande på MSN om via msnboten:<br />' . "\n";
 			echo '<form action="post">' . "\n";
@@ -18,10 +27,18 @@
 			echo '</ul>' . "\n";
 			echo '<input type="button" value="Spara" class="button_60" />' . "\n";
 			echo '</form>' . "\n";
+			*/
 		}
 		else
 		{
-			echo 'Adda hamsterpaj.net@live.se på MSN och skriv något till honom så svarar han med en länk. Klicka på den (eller kopiera den och öppna i webbläsaren om det inte funkar).';
+			if($_SESSION['userinfo']['msnbot_msn'] == '')
+			{
+				echo 'Adda hamsterpaj.net@live.se på MSN och skriv något till honom så svarar han med en länk. Klicka på den (eller kopiera den och öppna i webbläsaren om det inte funkar).';
+			}
+			else
+			{
+				echo '<a href="/installningar/msnbot_register.php">Avregistrera mig ifrån vännernotiser på MSN.</a>';
+			}
 		}
 	}
 	else
