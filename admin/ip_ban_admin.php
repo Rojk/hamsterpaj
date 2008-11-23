e857259f47b0dd2796bc7d778eb70442030465c7
Joar Wandborg <joar@hamsterpaj.net>
2008-11-23 16:03:31 +0100

    MAJNOR FIKKS(utropstekken)
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- admin/ip_ban_admin.php
+++ admin/ip_ban_admin.php
@@ -28,7 +28,7 @@
 			$out .= '<td><strong>Utslängd av</strong></td>';
 			$out .= '<td><strong>Ta bort</strong></td>' . "\n";
 			
-			$query = 'SELECT i.ip AS ip, i.reason AS reason, i.timestamp AS timestamp, l.username AS banned_by_username, l.id AS banned_by_user_id FROM ip_ban_list AS i, login AS l WHERE i.banned_by = l.id ORDER BY i.ip';
+			$query = 'SELECT i.ip AS ip, i.reason AS reason, i.timestamp AS timestamp, l.username AS banned_by_username, l.id AS banned_by_user_id FROM ip_ban_list AS i, login AS l WHERE i.banned_by = l.id ORDER BY i.timestamp';
 			$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);
 			while($data = mysql_fetch_assoc($result))
 			{
