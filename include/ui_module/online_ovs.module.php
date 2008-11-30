120e5220e55c4afacc676f4ae1fefe21e74efe10
hamsterpaj <johan@hamsterpaj.net>
2008-11-30 21:53:02 +0100

    Made OVs online ui_module work
    Signed-off-by: hamsterpaj <johan@hamsterpaj.net>

--- include/ui_module/online_ovs.module.php
+++ include/ui_module/online_ovs.module.php
@@ -4,22 +4,9 @@
 		$query = query_cache(array('query' => 'SELECT l.username AS username, l.id AS user_id FROM login AS l, privilegies AS pl WHERE pl.user = l.id AND pl.privilegie = "user_management_admin" ORDER BY lastaction DESC LIMIT 5'));
 		foreach($query AS $row)
 		{
-			if($row['lastaction'] > time() - 600)
-			{
-				$out .= '<li><a href="/traffa/profile.php?user_id=' . $row['id'] . '">' . $row['username'] . '</a></li>' . "\n";
-				
-				$many[] = 'Igge the new guy!'; //Random data, no matter what value it is
-			}
+			$out .= '<li><a href="/traffa/profile.php?user_id=' . $row['id'] . '">' . $row['username'] . '</a></li>' . "\n";
 		}
-		$count = count($many);
-		if ($count > 0)
-		{
-			return $out;
-		}
-		else
-		{
-			return false;
-		}
+		return $out;
 	}
 	
 	$options['output'] .= '<div class="ovlist">' . "\n";
