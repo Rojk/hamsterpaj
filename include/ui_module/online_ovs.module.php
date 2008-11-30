e23c0de72a89852aa4edeb3d25655ff737629cb8
hamsterpaj <johan@hamsterpaj.net>
2008-11-30 22:31:30 +0100

    Made links in online ovs module work
    Signed-off-by: hamsterpaj <johan@hamsterpaj.net>

--- include/ui_module/online_ovs.module.php
+++ include/ui_module/online_ovs.module.php
@@ -4,7 +4,7 @@
 		$query = query_cache(array('query' => 'SELECT l.username AS username, l.id AS user_id FROM login AS l, privilegies AS pl WHERE pl.user = l.id AND pl.privilegie = "user_management_admin" ORDER BY lastaction DESC LIMIT 5'));
 		foreach($query AS $row)
 		{
-			$out .= '<li><a href="/traffa/profile.php?user_id=' . $row['id'] . '">' . $row['username'] . '</a></li>' . "\n";
+			$out .= '<li><a href="/traffa/profile.php?user_id=' . $row['user_id'] . '">' . $row['username'] . '</a></li>' . "\n";
 		}
 		return $out;
 	}
