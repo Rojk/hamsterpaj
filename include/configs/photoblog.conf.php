2f9175928d757e631e7f51a2f7da0bd8342e6591
Joar Wandborg <joar@hamsterpaj.net>
2008-12-09 01:03:30 +0100

    I don't even remember what i've changed, but i guess i was succeeding from my last commit ;)
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

---
+++ include/configs/photoblog.conf.php
@@ -0,0 +1,8 @@
+<?php
+	$photoblog_preferences_default_values = array(
+		'user_id' => $_SESSION['login']['id'],
+		'color_main' => 'FFFF00',
+		'color_detail' => 'FF00FF',
+		'hamster_guard_on' => 0
+	);
+?>
--- include/libraries/photoblog_preferences.lib.php
+++ include/libraries/photoblog_preferences.lib.php
@@ -1,12 +1,10 @@
 <?php
 	class photoblog_preferences
 	{
-		$photoblog_preferences_default_values['color_main'] = 'FFFF00';
-		$photoblog_preferences_default_values['color_detail'] = 'FF00FF';
-		$photoblog_preferences_default_values['hamster_guard'] = 0;
-		function fetch($user_id)
+		function fetch($user_id, $photoblog_preferences_default_values)
 		{
-			global $photoblog_preferences_default;
+			global $photoblog_preferences_default_values;
+			$user_id = (!empty($user_id)) ? $user_id : $_SESSION['login']['id'];
 			if(!is_numeric($user_id))
 			{
 				throw new Exception('$user_id must be a numerical value');
@@ -39,15 +37,47 @@
 						$photoblog_preferences_init_sql .= ',';
 					}
 				}
+				mysql_query($photoblog_preferences_init_sql) or report_sql_error($photoblog_preferences_init_sql, __FILE__, __LINE__);
+				$photoblog_preferences_fetch_data = $photoblog_preferences_default_values;
 			}
-			$photoblog_preferences_fetch_data = mysql_fetch_assoc($photoblog_preferences_fetch_result);
+			else
+			{
+				$photoblog_preferences_fetch_data = mysql_fetch_assoc($photoblog_preferences_fetch_result);
+			}
 			
 			return $photoblog_preferences_fetch_data;
 		}
 		
-		function save($values)
+		function save($photoblog_preferences_save_values)
 		{
-			$photoblog_preferences_save_sql = 'UPDATE ';
+			$photoblog_preferences_save_sql = 'UPDATE photoblog_preferences WHERE user_id = ' . $_SESSION['login']['id'] . ' SET';
+			$photoblog_preferences_save_values_count = count($photoblog_preferences_save_values);
+			$count = 0;
+			foreach ($photoblog_preferences_save_values as $values_key => $values_val)
+			{
+				if (is_numeric($values_val))
+				{
+					$photoblog_preferences_save_sql .= ' ' . $values_key . ' = ' . $values_val;
+				}
+				else
+				{
+					$photoblog_preferences_save_sql .= ' ' . $values_key . ' = "' . $values_val . '"';
+				}
+				$count++;
+				if ($count != $photoblog_preferences_save_values_count)
+				{
+					$photoblog_preferences_save_sql .= ',';
+				}
+			}
+			if (mysql_query($photoblog_preferences_save_sql))
+			{
+				return true;
+			}
+			else
+			{
+				report_sql_error($photoblog_preferences_save_sql, __FILE__, __LINE__);
+				return false;
+			}
 		}
 	}
 ?>
