657abb2f4fed87d7dfe1c870feed48c258dc0f63
Hamsterpaj <johan@hamsterpaj.net>
2008-12-17 18:58:59 +0100

    Added iCloud ad
    Signed-off-by: Hamsterpaj <johan@hamsterpaj.net>

--- include/libraries/ui.lib.php
+++ include/libraries/ui.lib.php
@@ -454,7 +454,15 @@ function ui_bottom($options = array())
 	$output .= '<script type="text/javascript">CM8ShowAd("Skyscraper");</script>' . "\n";
 	$output .= '</div>' . "\n";
 	
-	$output .= '<div id="fiskpinne">' . "\n";
+	$output .= '<div id="fiskpinne" style="background: none;">' . "\n";
+	$output .= '<div id="cloudad" style="background: none;"></div>
+						<script type="text/javascript">
+						var s1 = new SWFObject("http://images.hamsterpaj.net/cloudad.swf","single","160","600","7");
+						s1.addVariable("width","160");
+						s1.addVariable("height","600");
+						s1.write("cloudad");
+						</script>';
+	$output .= '<div style="height: 50px;"></div>' . "\n";
 	$output .= '<script type="text/javascript" src="http://www.adtrade.net/ad/p/?id=hamsterpaj_1&size=140x350&ad=001" charset="iso-8859-1"></script>';
 	$output .= '</div>' . "\n";
 	
