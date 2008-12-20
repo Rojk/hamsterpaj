f4e2a2b5a8d2f08dceb7c83c4249f2ad3066e900
Patrick Lef <patrick@hamsterpaj.net>
2008-12-20 10:56:10 +0100

    Removed byt visningsbild-title from avatar in ui
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/libraries/ui.lib.php
+++ include/libraries/ui.lib.php
@@ -221,7 +221,7 @@ function ui_top($options = array())
 		$output .= '			</div>' . "\n";
 		
 		$output .= '			<div id="ui_statusbar">' . "\n";
-		$output .= '				<a href="#" title="Byt visningsbild">' . "\n";
+		$output .= '				<a href="#">' . "\n";
 		$output .= '					<img src="http://images.hamsterpaj.net/images/users/thumb/' . $_SESSION['login']['id'] . '.jpg" alt="" onclick="window.open(\'/avatar.php?id=' . $_SESSION['login']['id'] . '\',\'' . rand() . '\',\'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=410, height=600\')"/>' . "\n";
 		$output .= '				</a>' . "\n";
 		$output .= '				<div id="ui_statusbar_username">' . "\n";
