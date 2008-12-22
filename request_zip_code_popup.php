dc9657c81eae163c864d71f9e39acb1f03b92895
Joar Wandborg <joar@hamsterpaj.net>
2008-12-22 03:25:20 +0100

    Fixed syntax errors in request_zip_code_popup.php

--- request_zip_code_popup.php
+++ request_zip_code_popup.php
@@ -1,9 +1,9 @@
 <?php
-if (isset($_POST['submit'])
+if (isset($_POST['submit']))
 {
 	if (is_numeric($_POST['zip_code']))
 	{
-		$query = 'UPDATE userinfo SET zip_code = "' . $_POST['zip_code'] . '" WHERE userid = "' . $_SESSION['login']['id'] . '";
+		$query = 'UPDATE userinfo SET zip_code = "' . $_POST['zip_code'] . '" WHERE userid = "' . $_SESSION['login']['id'] . '"';
 		mysql_query($query);
 	}
 	echo 'Tack för ditt postnummer';	
@@ -13,6 +13,6 @@ else
 	echo '<form method="post" target="' . $_SERVER['PHP_SELF'] . '">';
 	echo '<input type="text" name="zip_code" />';
 	echo '<input type="submit" value="Spara!" name="submit">';
-	echo '</form'>';
+	echo '</form>';
 }
 ?>
