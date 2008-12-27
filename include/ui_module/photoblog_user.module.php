7b6d4f14c579ff74b21769d916570ec14f65c5d9
Joar Wandborg <joar@hamsterpaj.net>
2008-12-27 23:59:23 +0100

    Further work on photoblog, hopefully made samethings work.
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- include/ui_module/photoblog_user.module.php
+++ include/ui_module/photoblog_user.module.php
@@ -1,5 +1,5 @@
 <?php
-	$options['output'].= ui_avatar('3');
-	$options['output'].= '<h3>iPhone p13</h3>' . "\n";
-	$options['output'].= '<br /><a href="/traffa/profile.php?user_id=3">Gå till presentation >></a>' . "\n";
+	$options['output'].= ui_avatar($options['photoblog_current_view_user_id']);
+	$options['output'].= '<h3>' . $options['photoblog_current_view_username'] . '</h3>' . "\n";
+	$options['output'].= '<br /><a href="/traffa/profile.php?user_id=' . $options['photoblog_current_view_user_id'] . '">Gå till presentation &raquo;</a>' . "\n";
 ?>
