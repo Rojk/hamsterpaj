a33c45b970973e576d900e14ee3f86de849745c3
Patrick Lef <patrick@hamsterpaj.net>
2009-01-03 16:17:10 +0100

    Fixed so album module in photoblog shows users albums
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- fotoblogg/album.php
+++ fotoblogg/album.php
@@ -6,17 +6,29 @@
 		if ( isset($uri_parts[4]) && preg_match('/^[a-zA-Z0-9-_]+$/', $uri_parts[4]) )
 		{
 			$albumname = $uri_parts[4];	
-			if ( $uri_parts[5] == 'list' )
-			{
-				$out .= 'Visar album ' . $albumname . 's foton.';
-			}
-			else
-			{
-				$out .= 'Visar foton i albumet: ' . $albumname . '';
-			}
+			$out .= 'Visar album ' . $albumname . 's foton.';
+				global $photoblog_user;
+				$options['name'] = $albumname;
+				$photoblog_album = photoblog_categories_fetch($options);
+				
+				preint_r($photoblog_album, true);
 		}
 		else
 		{		
-				$out .= 'Listar ' . $photoblog_user['username'] . 's foton.';
+				$out .= 'Listar ' . $photoblog_user['username'] . 's album.';
+				global $photoblog_user;
+				$options['user'] = $photoblog_user['id'];
+				$photoblog_albums = photoblog_categories_fetch($options);
+				
+				foreach($photoblog_albums as $photoblog_album)
+				{
+					if(count($photoblog_album['photos']) >= 1)
+					{
+						$out .= '<a href="/fotoblogg/' . $photoblog_user['username'] . '/album/' . $photoblog_album['name'] . '" />' . "\n";
+						$out .= '<img src="http://images.hamsterpaj.net/photos/full/' . floor($photoblog_album['photos']['id'][0]/5000) . '/' . $photoblog_album['photos']['id'][0] . '.jpg" />' . "\n";
+						$out .= '<h3>' . $photoblog_album['name'] . '</h3>' . "\n";
+						$out .= '</a>' . "\n";
+					}
+				}
 		}
 ?>
