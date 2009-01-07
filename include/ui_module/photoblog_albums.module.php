e570533cfbe865d99aafd9ac0025f1b1338795f3
Patrick Lef <patrick@hamsterpaj.net>
2009-01-07 14:38:01 +0100

    Made album thumbnails in photoblog use thumbnails instead of full images
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/ui_module/photoblog_albums.module.php
+++ include/ui_module/photoblog_albums.module.php
@@ -8,7 +8,7 @@
 		{
 			$photoblog_album['photos'] = explode(',', $photoblog_album['photos']);
 			$options['output'].= '<a href="/fotoblogg/' . $photoblog_user['username'] . '/album/' . $photoblog_album['name'] . '" />' . "\n";
-			$options['output'].= '<img src="http://images.hamsterpaj.net/photos/full/' . floor($photoblog_album['photos'][0]/5000) . '/' . $photoblog_album['photos'][0] . '.jpg" />' . "\n";
+			$options['output'].= '<img src="http://images.hamsterpaj.net/photos/thumb/' . floor($photoblog_album['photos'][0]/5000) . '/' . $photoblog_album['photos'][0] . '.jpg" />' . "\n";
 			$options['output'].= '<h3>' . $photoblog_album['name'] . '</h3>' . "\n";
 			$options['output'].= '</a>' . "\n";
 		}
