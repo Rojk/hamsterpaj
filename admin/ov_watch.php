a053c3f8cad790ead242c792adbeddeac0648311
Joar Wandborg <joar@hamsterpaj.net>
2009-01-11 02:17:38 +0100

    Fixed OV-watch to work with privilegies instead of... userlevels, that's it.
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- admin/ov_watch.php
+++ admin/ov_watch.php
@@ -9,49 +9,69 @@ require_once(PATHS_INCLUDE . 'libraries/markup.php');
 //require_once(PATHS_INCLUDE . 'libraries/tips.lib.php');
 
 $ui_options['menu_path'] = array('admin', 'ov_watch');
+$ui_options['stylesheets'][] = 'forms.css';
 
-ui_top($ui_options);
 
 if(!is_privilegied('use_statistic_tools'))
 {
-	echo 'inte för dig...';
+	$out .= 'inte för dig...';
 }
 else
 {
-
-	$query = 'SELECT * FROM login l, admin_counts ac WHERE l.userlevel > 2 AND l.id = ac.user_id ORDER BY l.username';
-	$result = mysql_query($query);
+	$query = 'SELECT user FROM privilegies GROUP BY(user)';
+	$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);
+	while ($data = mysql_fetch_assoc($result))
+	{
+		$privilegied_users[] = $data['user'];
+	}
+	
+	$query = 'SELECT l.id, l.username, posts_removed, avatars_denied, avatars_approved FROM login AS l, admin_counts AS ac WHERE (l.id = ac.user_id) AND (';
+	$privilegied_users_count = count($privilegied_users);
+	$count = 0;
+	foreach ($privilegied_users as $user_id)
+	{
+		$query .= ' l.id = ' . $user_id;
+		$count++;
+		if ($privilegied_users_count != $count)
+		{
+			$query .= ' OR';
+		}
+	}
+	$query .= ') ORDER BY l.username';
+	$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);
 	while($data = mysql_fetch_assoc($result))
 	{
 		$ovs[] = $data;
 	}
+	$out .= '<fieldset>' . "\n";
+	$out .= '<legend>Ordningsvakter och deras förehavanden</legend>' . "\n";
+	$out .= '<table class="form">' . "\n";
+	$out .= '<tr>' . "\n";
+	$out .= '<th>Namn</th>' . "\n";
+	$out .= '<th>Id</th>' . "\n";
+	$out .= '<th>Borttagna inlägg</th>' . "\n";
+	$out .= '<th>Nekade visningsbilder</th>' . "\n";
+	$out .= '<th>Validerade visningsbilder</th>' . "\n";
+	$out .= '<th>Andel nekade visningsbilder</th>' . "\n";
+	$out .= '</tr>' . "\n";
 	
-	echo '<h1>Ordningsvakter och deras förehavanden</h1>' . "\n";
-	echo '<table>' . "\n";
-	echo '<tr>' . "\n";
-	echo '<td>Namn</td>' . "\n";
-	echo '<td>Id</td>' . "\n";
-	echo '<td>Borttagna inlägg</td>' . "\n";
-	echo '<td>Nekade visningsbilder</td>' . "\n";
-	echo '<td>Validerade visningsbilder</td>' . "\n";
-	echo '<td>Andel nekade visningsbilder</td>' . "\n";
-	echo '</tr>' . "\n";
 	foreach($ovs as $ov)
 	{
-		echo '<tr>' . "\n";
-		echo '<td>' . $ov['username'] . '</td>' . "\n";
-		echo '<td>' . $ov['id'] . '</td>' . "\n";
-		echo '<td>' . $ov['posts_removed'] . '</td>' . "\n";
-		echo '<td>' . $ov['avatars_denied'] . '</td>' . "\n";
-		echo '<td>' . $ov['avatars_approved'] . '</td>' . "\n";
-		// echo '<td>' . $ov['avatars_denied'] / ($ov['avatars_denied'] + $ov['avatars_approved']) . '</td>' . "\n";
-		echo '<td>' . round($ov['avatars_denied'] / ($ov['avatars_denied'] + $ov['avatars_approved']) * 100, 2).' % </td>' . "\n";
-		echo '</tr>' . "\n";
+		$out .= '<tr>' . "\n";
+		$out .= '<td>' . $ov['username'] . '</td>' . "\n";
+		$out .= '<td>' . $ov['id'] . '</td>' . "\n";
+		$out .= '<td>' . $ov['posts_removed'] . '</td>' . "\n";
+		$out .= '<td>' . $ov['avatars_denied'] . '</td>' . "\n";
+		$out .= '<td>' . $ov['avatars_approved'] . '</td>' . "\n";
+		// $out .= '<td>' . $ov['avatars_denied'] / ($ov['avatars_denied'] + $ov['avatars_approved']) . '</td>' . "\n";
+		$out .= '<td>' . round($ov['avatars_denied'] / ($ov['avatars_denied'] + $ov['avatars_approved']) * 100, 2).' % </td>' . "\n";
+		$out .= '</tr>' . "\n";
 	}
-	echo '</table>' . "\n";
+	$out .= '</table>' . "\n";
 }	
 
-//preint_r($_SESSION);
+ui_top($ui_options);
+echo $out;
 ui_bottom();
 
 ?>
