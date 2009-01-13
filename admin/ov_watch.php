98170f44568571d4483965b06ef8175f55abd6e3
Joar Wandborg <joar@hamsterpaj.net>
2009-01-13 18:49:37 +0100

    Fixed the creating daily poll hack. and something more, wich i don't remember what it was :>
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- admin/ov_watch.php
+++ admin/ov_watch.php
@@ -3,10 +3,6 @@
 require('../include/core/common.php');
 require_once(PATHS_INCLUDE . 'libraries/posts.php');
 require_once(PATHS_INCLUDE . 'libraries/markup.php');
-//require_once($hp_includepath . '/libraries/markup.php');
-//require_once($hp_includepath . '/libraries/games.lib.php');
-//require_once($hp_includepath . '/libraries/schedule.lib.php');
-//require_once(PATHS_INCLUDE . 'libraries/tips.lib.php');
 
 $ui_options['menu_path'] = array('admin', 'ov_watch');
 $ui_options['stylesheets'][] = 'forms.css';
