70e34444b738e199d7dd530834e38c1b8d0eec77
Joel <joel@hamsterpaj.net>
2009-01-18 02:29:03 +0100

    Removed old rubbish from database_init.php
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- include/core/database_init.php
+++ include/core/database_init.php
@@ -2,31 +2,6 @@
 //	include(PATHS_INCLUDE . 'downtime.php');
 //	exit;
 
-	function debug_trace($message)
-	{
-		echo $message . '<br />';
-		ob_flush();
-		flush();
-	}
-	
-	function debug_time($position)
-	{
-		global $debug_time;
-		
-		$mtime = microtime();
-		$mtime = explode(" ",$mtime);
-		$mtime = $mtime[1] + $mtime[0];
-
-		if(count($debug_time) > 0)
-		{
-			$debug_time[] = array($position => ($mtime - $debug_time[0]['Script start']) );
-		}
-		else
-		{	
-			$debug_time[] = array($position => $mtime);
-		}
-	}
-
 	//Backup is running and slowing down everything between 4.30 and ca 5.00
 	if(date('H') == '4' && date('i') > 30)
 	{
