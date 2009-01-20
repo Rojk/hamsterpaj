5933301162d7dc32e6af11b2b413ba1e352c4d7d
Joel <joel@hamsterpaj.net>
2009-01-20 18:40:10 +0100

    Added some escaping security to database.lib.php. You never know..
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- include/libraries/database.lib.php
+++ include/libraries/database.lib.php
@@ -4,7 +4,7 @@ function report_sql_error($query, $file = null, $line = null)
   echo '<div class="server_message_error"><h2>Såhär skall det ju inte bli, usch!</h2><p>Ett fel på hamsterpaj har inträffat! Utvecklingsansvariga har meddelats om detta, du behöver inte rapportera felet. Vi åtgärdar det snart (om vi kan :P)</p>';
 	echo '<h3 class="server_message_collapse_header" id="server_message_collapse_header_sqlerror">Visa felsökningsinformation</h3>' . "\n";
   echo '<div class="server_message_collapsed_information" id="server_message_collapse_information_sqlerror">' . "\n";
-  echo '<br />Felsökningsinformation:<br />' . mysql_error();
+  echo '<br />Felsökningsinformation:<br />' . htmlspecialchars(mysql_error());
   echo '<br />Frågan löd:<br /><p>' . htmlspecialchars($query) . '</p>';
   echo $file . ' #' . $line;
  	echo '<h1>Backtrace</h1>' . "\n";
