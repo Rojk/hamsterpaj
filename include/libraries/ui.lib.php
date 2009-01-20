afdad8df411ca726b8f9dc56d6f3e0c6eb3ec585
Joel <joel@hamsterpaj.net>
2009-01-20 18:47:37 +0100

    Moved ui_dropbox to ui.lib.php.
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- include/libraries/ui.lib.php
+++ include/libraries/ui.lib.php
@@ -770,4 +770,40 @@ function ui_module_render($options)
 		return $output;
 	}
 	
+	function ui_dropbox($title, $data, $styleinfo, $expanded = NULL)
+	{
+		$rand = rand(100000, 999999);
+		$return = '<div';
+		if (array_key_exists('class', $styleinfo))
+		{
+			$return.= ' class="' . $styleinfo['class'] . '"';
+		}
+		if (array_key_exists('style', $styleinfo))
+		{
+			$return.= ' style="' . $styleinfo['style'] . '"';
+		}
+		$return.= '>' . "\n";
+		$return.= '<div class="droptitle" onclick="collapse_expand(\'' . $rand . '\');">' . "\n";
+	  	$return.= '<h2 style="margin-top: 0;">' . $title . '</h2>' . "\n";
+//		$return.= '<img class="dropimage" id="dropbox_image_' . $rand . '" src="http://images.hamsterpaj.net/famfamfam_icons/bullet_toggle_';
+//		if (isset($expanded)) {
+//			$return.= 'minus';
+//		}
+//		else
+//			{
+//			$return.= 'plus';
+//		}
+//		$return.= '.png" alt="" />' . "\n";
+		$return.= '</div>' . "\n";
+		$return.= '<div id="dropbox_' . $rand . '"';
+		if (!isset($expanded))
+		{
+			$return.= ' style="display: none;"';
+		}
+		$return.= '>' . "\n";
+		$return.= $data;
+		$return.= '</div>' . "\n";
+		$return.= '</div>' . "\n";
+		return $return;
+	}
 ?>
