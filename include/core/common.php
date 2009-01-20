e28116fd2b31b393f451185df9669eb8f66acf18
Joel <joel@hamsterpaj.net>
2009-01-20 18:57:26 +0100

    Auto-including database.lib.php in core from now.
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- include/core/common.php
+++ include/core/common.php
@@ -31,6 +31,7 @@
 	define('CORE_PATH', substr(__FILE__, 0, strrpos(__FILE__, '/')+1));
 
   require_once(CORE_PATH . 'constants.php');
+  require_once(PATHS_INCLUDE . 'libraries/database.lib.php');
 	require_once(CORE_PATH . 'database_init.php');
 	require_once($hp_includepath . 'logging-functions.php');
   require(PATHS_INCLUDE . 'libraries/login.lib.php');
