7ebeb779e5830642eab553053b10a08756cda173
Joel <joel@hamsterpaj.net>
2009-01-24 21:29:41 +0100

    Removed old right_modules/ from include/. ui_module/ is used in Amanda.
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- include/right_modules/ad_1.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$return .= '<ul>' . "\n";
-	$return .= '<li><img src="http://imp.affiliator.com/imp.php?a=7368&b=1952&w=10193&p=147" width="0" height="0" /><a href="http://click.affiliator.com/click/a/7368/b/1952/w/10193/p/147/" target="_blank">Bästa musiken gratis till mobilen!</a></li>' . "\n";
-	$return .= '<li><img src="http://imp.affiliator.com/imp.php?a=7368&b=151&w=10193&p=35" width="0" height="0" /><a href="http://click.affiliator.com/click/a/7368/b/151/w/10193/p/35/" target="_blank">Köp kondomer med fri frakt och diskret avsändare!</a></li>' . "\n";
-	$return .= '<li><img src="http://imp.affiliator.com/imp.php?a=7368&b=3118&w=10193&p=180" width="0" height="0" /><a href="http://click.affiliator.com/click/a/7368/b/3118/w/10193/p/180/" target="_blank">Spara upp till 50% på din märkesparfym. Handla tryggt mot faktura!</a></li>' . "\n";
-	$return .= '<li><img src="http://imp.affiliator.com/imp.php?a=7368&b=590&w=10193&p=60" width="0" height="0" /><a href="http://click.affiliator.com/click/a/7368/b/590/w/10193/p/60/" target="_blank">På SMS.se får alla medlemmar skicka gratis SMS varje dag.</a></li>' . "\n";
-	$return .= '</ul>' . "\n";
-?>
