b481554c74be9bd7d5fff9b7e17656673a3aa017
Patrick Lef <patrick@hamsterpaj.net>
2009-01-25 20:15:06 +0100

    Friends_notices uses now is_numeric instead of intval. Joel says you shouldn't use intval, because it doesn't work that way.
    Now sad panda is gonna eat chocolate instead :(
    
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- ajax_gateways/friends_notices_remove_all_from_user.php
+++ ajax_gateways/friends_notices_remove_all_from_user.php
@@ -1,7 +1,7 @@
 <?php
 	require('../include/core/common.php');
 	
-	$options['friend_id'] = intval($_GET['friend_id']) ? $_GET['friend_id'] : die('no valid id');
+	$options['friend_id'] = is_numeric($_GET['friend_id']) ? $_GET['friend_id'] : die('no valid id');
 	friends_notices_remove($options);
 	echo 'lol';
 	unset($_SESSION['friends_actions'][$options['friend_id']]);
