0e400b174308186d7eb9bb9f55d922210361d8dd
Joel <joel@hamsterpaj.net>
2009-01-26 19:44:18 +0100

    Removed old included stuff from ov_reg.php...
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- admin/ov_reg.php
+++ admin/ov_reg.php
@@ -1,12 +1,6 @@
 <?php
 	
 require('../include/core/common.php');
-require_once(PATHS_INCLUDE . 'libraries/posts.php');
-require_once(PATHS_INCLUDE . 'libraries/markup.php');
-//require_once($hp_includepath . '/libraries/markup.php');
-//require_once($hp_includepath . '/libraries/games.lib.php');
-//require_once($hp_includepath . '/libraries/schedule.lib.php');
-//require_once(PATHS_INCLUDE . 'libraries/tips.lib.php');
 
 $ui_options['menu_path'] = array('admin', 'ov_reg');
 
