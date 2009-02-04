da1a9941dcb50786ac11c7a4ca9bd5a98bb76499
Erik Andersson <erik.rothoff@gmail.com>
2009-02-04 18:55:17 +0100

    Fixed photoblog calander. Now it works correctly with January, atleast.
    Signed-off-by: Erik Andersson <erik.rothoff@gmail.com>

--- include/ui_module/photoblog_calendar.module.php
+++ include/ui_module/photoblog_calendar.module.php
@@ -5,33 +5,33 @@
 		$used_dates = photoblog_dates_fetch($options);
 		$used_dates = $used_dates[$year][$month];
 		
-		preint_r(func_get_args());
-		
 		$date = mktime(12, 0, 0, $month, 1, $year);
 		$daysInMonth = date('t', $date);
-
+		
 		$offset = date('N', $date);
 		$rows = 1;
 		$out .= '<div id="photoblog_calendar_month">' . "\n";
 			$out.= '<a href="">&laquo;</a>' . "\n";
-				$out .= '<span>' . date('F', $date) . ', ' . $year . '</span>' . "\n";
+				$out .= '<span title="offset: ' . $offset . '">' . date('F', $date) . ', ' . $year . '</span>' . "\n";
 			$out.= '<a href="">&raquo;</a>' . "\n";
 		$out .= '</div>' . "\n";
 		$out .= '<table>' . "\n";
 		$out .= '<tr><th>M</th><th>T</th><th>O</th><th>T</th><th>F</th><th>L</th><th>S</th></tr>' . "\n";
 		$out .= '<tr>';
-		for($i = 1; $i <= $offset; $i++)
+		for($i = 1; $i < $offset; $i++)
 		{
 			$out .= '<td></td>' . "\n";
 		}
 		for($day = 1; $day <= $daysInMonth; $day++)
 		{
-			if( ($day + $offset) % 7 == 0 && $day != 1)
+			$format_day = ($day < 10) ? '0' . $day : $day;
+			
+			if( ($day + $offset - 2) % 7 == 0 && $day != 1)
 			{
 				$out .= '</tr><tr>' . "\n";
 				$rows++;
 			}
-			$out .= '<td>' . (isset($used_dates[$day]) ? '<a href="#day-' . $year . $month . $day . '">' . $day . '</a>' : $day) . '</td>' . "\n";
+			$out .= '<td>' . (isset($used_dates[$format_day]) ? '<a href="#day-' . $year . $month . $day . '">' . $day . '</a>' : $day) . '</td>' . "\n";
 		}
 		while( ($day + $offset) <= $rows * 7)
 		{
