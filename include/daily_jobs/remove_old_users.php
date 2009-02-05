16318ffbe094e4ce3defed6aaf04bb845ee72570
Patrick Lef <patrick@hamsterpaj.net>
2009-02-05 11:40:53 +0100

    Fixed so user Anonym does not get removed with daily jobs
    Signed-off-by: Patrick Lef <patrick@hamsterpaj.net>

--- include/daily_jobs/remove_old_users.php
+++ include/daily_jobs/remove_old_users.php
@@ -1,6 +1,6 @@
 <?php
 	/* Remove old users */
-	$query = 'SELECT id, username FROM login WHERE (lastlogon < UNIX_TIMESTAMP() - 60*60*24*183 OR lastlogon IS NULL) AND is_removed = "0" AND id != 857929 AND id != 2348'; // We don't wish to remove webmaster or tha hamster
+	$query = 'SELECT id, username FROM login WHERE (lastlogon < UNIX_TIMESTAMP() - 60*60*24*183 OR lastlogon IS NULL) AND is_removed = "0" AND id != 857929 AND id != 2348 AND id != 876354'; // We don't wish to remove webmaster or tha hamster or anonym
 	$result = mysql_query($query);
 	while($data = mysql_fetch_assoc($result))
 	{
