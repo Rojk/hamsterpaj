0c2bc0cf47d72040b29f4cfff05889f77cf21e99
Joar Wandborg <joar@hamsterpaj.net>
2009-02-09 00:48:42 +0100

    Prevented SQL injection.
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- ajax_gateways/forum_signature.php
+++ ajax_gateways/forum_signature.php
@@ -1,6 +1,6 @@
 <?php
 	require_once('../include/core/common.php');
-	$query = 'SELECT user_status FROM userinfo WHERE userid = ' . $_GET['id'];
+	$query = 'SELECT user_status FROM userinfo WHERE userid = ' . intval($_GET['id']);
 	$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);
 	$data = mysql_fetch_assoc($result);
 	echo $data['user_status'];
