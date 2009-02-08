2348504f9cf20ff3e815004a94e574272bba3ff8
Joar Wandborg <joar@hamsterpaj.net>
2009-02-08 17:05:31 +0100

    Updated newly_registered_users, both PHP and JS file.
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- admin/newly_registered_users.php
+++ admin/newly_registered_users.php
@@ -1,6 +1,11 @@
 <?php
 	include '../include/core/common.php';
 	$ui_options['title'] = 'Nyregistrerade användare - Hamsterpaj.net';
+	$ui_options['javascripts'][] = 'jquery.tooltip.js';
+	$ui_options['javascripts'][] = 'jquery.dimensions.js';
+	$ui_options['javascripts'][] = 'newly_registered_users.js';
+	$ui_options['stylesheets'][] = 'jquery.tooltip.css';
+	
 	
 	if (!is_privilegied('remove_user')) {
 		die('ARRGH! AGH! AAAARH ARGHAAAA *GACK*<br /><a href="/">Hjälp, jag vill hem D:</a>');
@@ -8,31 +13,58 @@
 	
 	$one_week_ago = time() - 60 * 60 * 24 * 7;
 	
-	$query = 'SELECT l.id, l.username, u.last_warning, l.regtimestamp FROM login l, userinfo u WHERE l.id = u.userid AND l.regtimestamp > ' . $one_week_ago . ' ORDER BY l.regtimestamp DESC';
+	$query = 'SELECT l.id, l.regtimestamp, l.username, u.last_warning, l.quality_level, l.quality_level_expire, t.guestbook_entries, u.forum_posts';
+	$query .= ' FROM login l, userinfo u, traffa t';
+	$query .= ' WHERE l.id = t.userid AND l.id = u.userid AND l.regtimestamp > ' . $one_week_ago;
+	$query .= ' ORDER BY l.regtimestamp DESC';
 	$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);
 	$out = '<div style="font: 12px monospace;">
-	<table style="width: 638px;">
+	<style>
+	#ui_content * {
+		cell-padding: 0;
+		cell-spacing: 0;
+		
+		margin: 0;
+		padding: 0;
+	}
+	</style>
+	<table id="newly_registered_users" style="width: 638px;">
 	<tr>
 		<th>Registrerad</th>
 		<th>Användarnamn</th>
 		<th>Varnad?</th>
+		<th>Read only?</th>
 	</tr>
 		' . "\n";
 	while ($data = mysql_fetch_assoc($result)) {
 		$out .= '<tr style="background: #FAFAFA;">' . "\n";
 		$out .= '<td>' . date("Y.m.d H:i", $data['regtimestamp']) . '</td>' . "\n";
-		$out .= '<td><a href="/traffa/user_facts.php?user_id=' . $data['id'] . '">' . $data['username'] . '</a></td>' . "\n";
+		$out .= '<td><a class="user_info" id="' . $data['id'] . '" href="/traffa/user_facts.php?user_id=' . $data['id'] . '">' . $data['username'] . '</a></td>' . "\n";
+		$out .= '<span id="user_info_' . $data['id'] . '" style="display: none;">Foruminlägg: ' . $data['forum_posts'] . '<br />Gästboksinlägg: ' . $data['guestbook_entries'] . '</span>' . "\n";
 		$out .= '<td>';
 		if ($data['last_warning'] > 0) {
-			$out .= '<a title="' . fix_time($data['last_warning']) . '" style="color: red;" href="/admin/warnings.php?action=viewhistory&user_id=' . $data['id'] . '">Varnad</a>';
+			$out .= '<a class="user_warning" id ="' . $data['id'] . '" style="color: red;" href="/admin/warnings.php?action=viewhistory&user_id=' . $data['id'] . '">Varnad</a>';
+			$out .= '<span id="user_warning_' . $data['id'] . '" style="display: none;">' . fix_time($data['last_warning']) . '</span>';
 		}
 		else {
 			$out .= '<span style="color: #999;">Neup.</span>';
 		}
+		$out .= '<td>';
+		if ($data['quality_level_expire'] > time()) {
+			$out .= '<a style="color: red;" class="user_read_only" id="' . $data['id'] . '" href="/admin/user_management.php?username=' . $data['username'] . '">Jao</a>';
+			$out .= '<span id="user_read_only_' . $data['id'] . '" style="display: none;">';
+			$out .= 'Level ' . $data['quality_level'] . ', går ut ' . fix_time($data['quality_level_expire']);
+			$out .= '</span>';
+		}
+		else {
+			$out .= '<span style="color: #999;">Nepp! :D</span>';
+		}
+		$out .= '</td>' . "\n";
 		$out .= '</td>' . "\n";
 		$out .= '</tr>' . "\n";
 	}
 	$out .= '</table>' . "\n";
+	$out .= '</div>' . "\n";
 	
 	ui_top($ui_options);
 	echo $out;
