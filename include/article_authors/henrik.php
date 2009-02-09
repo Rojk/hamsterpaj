b979fb5f7bf4e2a618855fbb9c7bd7c3e699ae35
Hamsterpaj <johan@hamsterpaj.net>
2009-02-09 19:20:57 +0100

    Removed the article authors from the index (gitignored but still in the index).
    Signed-off-by: Hamsterpaj <johan@hamsterpaj.net>

--- include/article_authors/henrik.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$author['name'] = 'Henrik Almegren, utvecklare, Hamsterpaj AB';
-	$author['description'] = 'Henrik undervisade <a href="/traffa/profile.php?id=3">Johan</a> i "IT-säkerhet och datorkommunikation" på Portalens Gymnasium i Göteborg. Efter lite om och men så hamnade han istället som utvecklare i Johans företag, Hamsterpaj AB. Henrik är född -77 och bor ute i obygden med fru och barn i sitt nybyggda hus. Henrik är nästan färdig civilingengjör från Chalmers och har specialiserat sig på systemutveckling.';
-	$author['image'] = 'henrik.jpg';
-?>
