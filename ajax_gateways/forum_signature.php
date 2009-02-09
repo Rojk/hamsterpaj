f0d9e2ff9e838342ae9d9ed39198ca57f5097aff
Joar Wandborg <joar@hamsterpaj.net>
2009-02-09 01:06:23 +0100

    Fixed the forum_signature gateway with a different solution.
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- ajax_gateways/forum_signature.php
+++ ajax_gateways/forum_signature.php
@@ -1,7 +1,12 @@
 <?php
-	require_once('../include/core/common.php');
-	$query = 'SELECT user_status FROM userinfo WHERE userid = ' . intval($_GET['id']);
-	$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);
-	$data = mysql_fetch_assoc($result);
-	echo $data['user_status'];
+	if (is_numeric($_GET['id'])) {
+		require_once('../include/core/common.php');
+		$query = 'SELECT user_status FROM userinfo WHERE userid = ' . $_GET['id'];
+		$result = mysql_query($query) or report_sql_error($query, __FILE__, __LINE__);
+		$data = mysql_fetch_assoc($result);
+		echo $data['user_status'];
+	}
+	else {
+		echo 'FülhackaRRRe! Üt uRR Mein HAUS!';
+	}
 ?>
