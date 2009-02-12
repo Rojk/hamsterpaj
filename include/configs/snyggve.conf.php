070fe835c9b786d4ff3f5bf5b195f53e1f4b9e2c
Joel <joel@hamsterpaj.net>
2009-02-12 14:20:48 +0100

    Replaced save path in snyggve conf.
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- include/configs/snyggve.conf.php
+++ include/configs/snyggve.conf.php
@@ -1,6 +1,6 @@
 <?php
-	define('SNYGGVE_TEMP_PATH', PATHS_IMAGES . 'snyggve_temp/');
+	define('SNYGGVE_TEMP_PATH', IMAGE_PATH . 'snyggve_temp/');
 	define('SNYGGVE_TEMP_URL', IMAGE_URL . 'snyggve_temp/');
-	define('SNYGGVE_PERM_PATH', PATHS_IMAGES . 'snyggve/');
+	define('SNYGGVE_PERM_PATH', IMAGE_PATH . 'snyggve/');
 	define('SNYGGVE_PERM_URL', IMAGE_URL . 'snyggve/');
 ?>
