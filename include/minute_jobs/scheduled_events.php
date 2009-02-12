481a63e62e56152221dfc1b4a461fe0b851ae62a
Joel <joel@hamsterpaj.net>
2009-02-12 19:28:10 +0100

    Removed fulkod from scheduled_events.php
    Signed-off-by: Joel <joel@hamsterpaj.net>

--- include/minute_jobs/scheduled_events.php
+++ include/minute_jobs/scheduled_events.php
@@ -1,14 +1,6 @@
 <?php
-        if(isset($_GET['manual']))
-        {
-                include('../core/common.php');
-                include(PATHS_INCLUDE . 'online_people_generator.php');
-                include(PATHS_INCLUDE . 'snyggve_gbc.php');     
-        }
-        if(isset($_GET['debug_include_standard'])){
-                include('../core/common.php');
-        }
-        require_once(PATHS_LIBRARIES . 'schedule.lib.php');
-
-        schedule_releases_do();
+	require_once('../core/common.php');
+	require_once(PATHS_LIBRARIES . 'schedule.lib.php');
+	
+	schedule_releases_do();
 ?>
