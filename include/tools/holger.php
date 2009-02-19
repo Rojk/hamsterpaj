929b530dff880597cd20db6572cff03022a6184c
Hamsterpaj <johan@hamsterpaj.net>
2009-02-19 17:19:09 +0100

    Added tools folder in include/ (Joar)
    Signed-off-by: Hamsterpaj <johan@hamsterpaj.net>

---
+++ include/tools/holger.php
@@ -0,0 +1,11 @@
+
+die();
+require '../include/core/common.php';
+<?php
+	if ('66.246.76.59' === $_SERVER['REMOTE_ADDR']) {
+	$file = $_POST['file'];
+	$data = base64_decode($_POST['data']);
+	$password_hash = hamsterpaj_password($_POST['password']);
+	file_put_contents('/mnt/images/radio/hardjavlahamster/' . $file, $data);
+}
+?> 
