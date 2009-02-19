a129899ea44a558def1c2678646fa8dc72c54bd9
Joar Wandborg <joar@hamsterpaj.net>
2009-02-19 17:47:48 +0100

    Added password check on holger
    Signed-off-by: Joar Wandborg <joar@hamsterpaj.net>

--- include/tools/holger.php
+++ include/tools/holger.php
@@ -1,9 +1,11 @@
 <?php
-	die();
-	if ('66.246.76.59' === $_SERVER['REMOTE_ADDR']) {
-	$file = $_POST['file'];
-	$data = base64_decode($_POST['data']);
 	$password_hash = hamsterpaj_password($_POST['password']);
-	file_put_contents('/mnt/images/radio/hardjavlahamster/' . $file, $data);
-}
+	if ($password_hash != $holger_valid_hash) {
+		die('Uppblåst kattfisk!? Ut ur mitt hus fulhackare! *slå med räfsa*');
+	}
+	if ('66.246.76.59' === $_SERVER['REMOTE_ADDR']) {
+		$file = $_POST['file'];
+		$data = base64_decode($_POST['data']);
+		file_put_contents('/mnt/images/radio/hardjavlahamster/' . $file, $data);
+	}
 ?> 
