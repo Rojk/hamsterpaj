9ee4604d73357b0d6d6dfbe194f2a44977e8ed93
Hamsterpaj <johan@hamsterpaj.net>
2009-03-15 21:20:24 +0100

    Moved live_chat/ cache directory to cache/.
    Signed-off-by: Hamsterpaj <johan@hamsterpaj.net>

--- include/configs/live_chat.conf.php
+++ include/configs/live_chat.conf.php
@@ -1,5 +1,5 @@
 <?php
-	define('LIVE_CHAT_STORAGE_PATH', PATHS_INCLUDE . 'live_chat/');
+	define('LIVE_CHAT_STORAGE_PATH', PATHS_CACHE . 'live_chat/');
 	define('LIVE_CHAT_MAX_READ_LINES', 25);
 	define('LIVE_CHAT_STORAGE_BUFFER_LENGTH', 100);
 ?>
